50a8c712303dc8432e76f3437ee176e024426e5b
GivingupCoke <2052346396@qq.com>
2024-10-25 12:54:24 +0800

    add files

---
+++ R_code/chap6/eg6-2.R
@@ -0,0 +1,12 @@
+flora <- read.table("flora.csv", header=TRUE, sep=",")
+attach(flora)
+proportion <- maple/(otherTrees+maple)
+sampleSize <- maple + otherTrees
+model_2a <- glm(proportion~elevation, family=binomial, weights=sampleSize)
+
+png("图6.1b.png", height = 800, width = 800)
+plot(elevation, proportion, las=1, pch=16, col="green", cex = 2, cex.axis=1.8, cex.lab=1.8)
+elevationPoints <- seq(min(elevation), max(elevation), by=0.01)
+fittedValues <- predict(model_2a, list(elevation=elevationPoints), type="response")
+lines(elevationPoints, fittedValues, lwd=3, col="blue")
+dev.off()
